d81587dea306e6f51fc9329679adc8cc104a9767
garetlekwah <phillipsflores7@gmail.com>
2025-08-22 17:51:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,20 @@ function toggleMenu() {
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+// Back to Top
+let backToTopButton = document.getElementById("backToTop");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+};
+
+backToTopButton.addEventListener("click", function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
 
 
 
