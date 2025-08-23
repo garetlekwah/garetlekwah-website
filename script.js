e5fd7074496f0d134fcef620e2cb9545933cf941
garetlekwah <phillipsflores7@gmail.com>
2025-08-22 17:06:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Hamburger Menu Toggle
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
+function toggleMenu() {
+  document.querySelector(".nav-links").classList.toggle("show");
+}
+
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
