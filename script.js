e08f1bf06a9d02689c4dd11e89f8b58779f52767
garetlekwah <phillipsflores7@gmail.com>
2025-08-22 18:24:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -82,3 +82,11 @@ backToTopButton.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("menu");
+
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+});
